88d97ba594ec77d7087afa8c80a4b8f7a183244b
Yuki Yasoshima <yukiyasos@gmail.com>
2014-10-25 13:36:48 +0900

    Play audio file.

--- AudioPlaygroundSample.playground/section-1.swift
+++ AudioPlaygroundSample.playground/section-1.swift
@@ -7,3 +7,20 @@ import XCPlayground
 XCPSetExecutionShouldContinueIndefinitely(continueIndefinitely: true)
 
 let audioEngine = AVAudioEngine()
+let outputNode = audioEngine.outputNode
+let format = audioEngine.outputNode.inputFormatForBus(0)
+
+let playerNode = AVAudioPlayerNode()
+audioEngine.attachNode(playerNode)
+
+audioEngine.connect(playerNode, to: outputNode, format: format)
+
+audioEngine.startAndReturnError(nil)
+
+playerNode.play()
+
+let path = XCPSharedDataDirectoryPath.stringByAppendingPathComponent("edge.wav")
+let url = NSURL.fileURLWithPath(path)
+let audioFile = AVAudioFile(forReading: url, error: nil)
+
+playerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
