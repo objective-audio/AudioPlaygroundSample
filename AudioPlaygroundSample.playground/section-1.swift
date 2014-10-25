7acf69b22b69c55a0aba8e4ee32f7da6a40e5f0b
Yuki Yasoshima <yukiyasos@gmail.com>
2014-10-25 13:37:34 +0900

    Play through.

--- AudioPlaygroundSample.playground/section-1.swift
+++ AudioPlaygroundSample.playground/section-1.swift
@@ -7,3 +7,10 @@ import XCPlayground
 XCPSetExecutionShouldContinueIndefinitely(continueIndefinitely: true)
 
 let audioEngine = AVAudioEngine()
+let inputNode = audioEngine.inputNode
+let outputNode = audioEngine.outputNode
+let format = audioEngine.outputNode.inputFormatForBus(0)
+
+audioEngine.connect(inputNode, to: outputNode, format: format)
+
+audioEngine.startAndReturnError(nil)
