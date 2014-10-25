48112bc36d885b81841b667b830dc9013a741508
Yuki Yasoshima <yukiyasos@gmail.com>
2014-10-25 21:38:16 +0900

    Sampler.

--- AudioPlaygroundSample.playground/section-1.swift
+++ AudioPlaygroundSample.playground/section-1.swift
@@ -7,3 +7,25 @@ import XCPlayground
 XCPSetExecutionShouldContinueIndefinitely(continueIndefinitely: true)
 
 let audioEngine = AVAudioEngine()
+let outputNode = audioEngine.outputNode
+let format = audioEngine.outputNode.inputFormatForBus(0)
+
+let samplerNode = AVAudioUnitSampler()
+audioEngine.attachNode(samplerNode)
+let path = NSBundle.mainBundle().pathForSoundResource("sine")
+let url: NSURL! = NSURL(fileURLWithPath: path!)
+let urls: [NSURL] = [url]
+samplerNode.loadAudioFilesAtURLs(urls, error: nil)
+
+audioEngine.connect(samplerNode, to: outputNode, format: format)
+
+audioEngine.startAndReturnError(nil)
+
+let seq: [UInt8] = [0, 2, 4, 5, 7, 9, 11, 12]
+while true {
+    for note in seq {
+        samplerNode.startNote(note + 60, withVelocity: 127, onChannel: 0)
+        NSThread.sleepForTimeInterval(0.5)
+    }
+}
+
